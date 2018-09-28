41dc89cd4fcf374b47d7c2232ae324abdb68e22d
DESKTOP-UE8LGV0\posat <posato_jia@hotmail.com>
2018-09-28 20:15:52 +0800

    去掉last name 後臺首頁文字

--- Presentation/Nop.Web/Validators/Customer/RegisterValidator.cs
+++ Presentation/Nop.Web/Validators/Customer/RegisterValidator.cs
@@ -33,7 +33,6 @@ namespace Nop.Web.Validators.Customer
             }
             
             RuleFor(x => x.FirstName).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.FirstName.Required"));
-            RuleFor(x => x.LastName).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.LastName.Required"));
 
 
             RuleFor(x => x.Password).NotEmpty().WithMessage(localizationService.GetResource("Account.Fields.Password.Required"));
